04a488bcb827ba51596ecee9d348686124326331
Andrey Savin <savin.andrey21@wb.ru>
2024-09-11 21:38:45 +0300

    added upd and del methods,removed global var

--- main.go
+++ main.go
@@ -6,33 +6,56 @@ import (
 	"net/http"
 )
 
-var message RequestBody
-
 func PostHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var message RequestBody
 	err := json.NewDecoder(r.Body).Decode(&message)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 	}
 
-	message.ID = 0
-
 	DB.Create(&message)
-
 	err = json.NewEncoder(w).Encode(message)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
-
 }
 
 func GetHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
 	var response []RequestBody
 	DB.Find(&response)
+
 	msgSlice := make([]string, len(response))
 	for index, value := range response {
 		msgSlice[index] = value.Message
 	}
 	json.NewEncoder(w).Encode(msgSlice)
+}
+
+func DeleteHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var response RequestBody
+	id := mux.Vars(r)["id"]
+	DB.First(&response, id)
+	DB.Delete(&response, id)
+
+	json.NewEncoder(w).Encode(response)
+}
+
+func UpdateHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var response RequestBody
+	id := mux.Vars(r)["id"]
+	DB.First(&response, id)
+	json.NewDecoder(r.Body).Decode(&response)
+	DB.Save(&response)
+
+	err := json.NewEncoder(w).Encode(response)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 }
 
 func main() {
@@ -42,5 +65,7 @@ func main() {
 	router := mux.NewRouter()
 	router.HandleFunc("/api/write", PostHandler).Methods("POST")
 	router.HandleFunc("/api/get", GetHandler).Methods("GET")
+	router.HandleFunc("/api/delete/{id}", DeleteHandler).Methods("DELETE")
+	router.HandleFunc("/api/update/{id}", UpdateHandler).Methods("PATCH")
 	http.ListenAndServe(":8080", router)
 }
